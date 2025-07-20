e89e1713f4f34b791143fd3dc121513e166e47fe
jeli01 <jeli01@naver.com>
2025-07-20 21:14:05 +0900

    refactor: XMLHttpRequest를 fetch로 마이그레이션

--- js/includeHTML.js
+++ js/includeHTML.js
@@ -1,25 +1,30 @@
 function includeHTML(callback) {
-    var z, i, elmnt, file, xhr;
+    var z, i, elmnt, file;
     z = document.getElementsByTagName("*");
     for (i = 0; i < z.length; i++) {
         elmnt = z[i];
         file = elmnt.getAttribute("include-html");
         if (file) {
-            xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = function() {
-                if (this.readyState == 4) {
-                    if (this.status == 200) {
-                        elmnt.innerHTML = this.responseText;
+            fetch(file)
+                .then(function(response) {
+                    if (response.status == 200) {
+                        return response.text();
+                    } else if (response.status == 404) {
+                        return "Page not found.";
+                    } else {
+                        return "";
                     }
-                    if (this.status == 404) {
-                        elmnt.innerHTML = "Page not found.";
-                    }
+                })
+                .then(function(text) {
+                    elmnt.innerHTML = text;
                     elmnt.removeAttribute("include-html");
                     includeHTML(callback);
-                }
-            };
-            xhr.open("GET", file, true);
-            xhr.send();
+                })
+                .catch(function() {
+                    elmnt.innerHTML = "Page not found.";
+                    elmnt.removeAttribute("include-html");
+                    includeHTML(callback);
+                });
             return;
         }
     }
